7bd5cd86586ac74cc8c8b7f1f374ea9c7526c718
DiegoMhz <dibzz456@gmail.com>
2023-11-17 20:10:01 -0400

    De prueba

--- app.js
+++ app.js
@@ -1,47 +1,100 @@
+// const express = require('express');
+// const { Pool } = require('pg')
+// const app = express();
+// const port = 3000;
+// const path = require('path');
+
+
+// const config = {
+//     user: "postgres",
+//     host: 'localhost',
+//     password: 'dube3342537',
+//     database: 'library'
+// }
+
+
+// const pool = new Pool(config);
+
+
+// app.use('/', express.static(path.join(__dirname, 'views')));
+
+// app.get('/books', async (req, res) => {
+//     try {
+//         const response = await pool.query('select * from books');
+//         libros = response.rows
+//         console.log(libros);
+//         res.json(libros);
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ error: 'Error al obtener los libros.' });
+//     }
+// });
+
+// app.get('/users', async (req, res) => {
+//     try {
+//         const response = await pool.query('select * from users');
+//         const users = response.rows
+//         res.json(users);
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ error: 'Error al obtener los usuarios.' });
+//     }
+// });
+
+// app.listen(port, () => {
+//     console.log(`Servidor escuchando en http://localhost:${port}`);
+// });
+// const { Client } = require('pg')
+// const obtenerdatos = async () => {
+
+//     const client = new Client({
+//         user: 'postgres',
+//         host: 'viaduct.proxy.rlwy.net',
+//         database: 'railway',
+//         password: '4C5DaBfga--ADEEAbB64GB1-aAc3gdDB',
+//         port: '12468',
+//     })
+//     await client.connect()
+
+//     const res = await client.query('select * from trabajadores')
+//     const resultado = res.rows
+//     console.log(resultado);
+//     await client.end()
+// }
+
+// obtenerdatos();
+
+const { Client } = require('pg');
 const express = require('express');
-const { Pool } = require('pg')
 const app = express();
 const port = 3000;
-const path = require('path');
 
+app.use(express.static('views')); // Carpeta para archivos estáticos (HTML)
 
-const config = {
-    user: "postgres",
-    host: 'localhost',
-    password: 'dube3342537',
-    database: 'library'
-}
+app.get('/obtenerdatos', async (req, res) => {
+  try {
+    const client = new Client({
+      user: 'postgres',
+      host: 'viaduct.proxy.rlwy.net',
+      database: 'railway',
+      password: '4C5DaBfga--ADEEAbB64GB1-aAc3gdDB',
+      port: '12468',
+    });
 
+    await client.connect();
 
-const pool = new Pool(config);
+    const result = await client.query('SELECT * FROM trabajadores');
+    const trabajadores = result.rows;
 
+    await client.end();
 
-app.use('/', express.static(path.join(__dirname, 'views')));
-
-app.get('/books', async (req, res) => {
-    try {
-        const response = await pool.query('select * from books');
-        libros = response.rows
-        res.json(libros);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Error al obtener los libros.' });
-    }
-});
-
-app.get('/users', async (req, res) => {
-    try {
-        const response = await pool.query('select * from users');
-        const users = response.rows
-        res.json(users);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Error al obtener los usuarios.' });
-    }
+    res.json(trabajadores);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Servidor escuchando en http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
-
-
--- views/index.js
+++ views/index.js
@@ -1,39 +1,18 @@
-
-const getBooks = async () => {
+// app.js
+document.addEventListener('DOMContentLoaded', async function () {
     try {
-        const response = await axios.get('https://prueba-5csg.onrender.com/books');
-        const books = response.data;
-        const list = document.getElementById('bookList');
-        books.forEach(book => {
+      const response = await fetch('/obtenerdatos');
+      const trabajadores = await response.json();
+      const listaTrabajadores = document.getElementById('lista-trabajadores');
+      trabajadores.forEach(trabajador => {
         const listItem = document.createElement('li');
-        listItem.textContent = `${book.title} - ${book.autor}`;
-        list.appendChild(listItem);
-    });
+        listItem.textContent = `ID: ${trabajador.id}, Nombre: ${trabajador.nombre}, Edad: ${trabajador.edad}`;
+        listaTrabajadores.appendChild(listItem);
+      });
     } catch (error) {
-        console.error('Error:', error);
+      console.error('Error:', error);
     }
-}
+  });
+  
 
 
-
-// Llama a la función cuando la página se carga
-getBooks();
-
-
-const getUser = async () => {
-    
-    try {
-        const response = await axios.get('https://prueba-5csg.onrender.com/users');
-        const users = response.data;
-        const list = document.getElementById('usersList');
-        users.forEach(user => {
-        const listItem = document.createElement('li');
-        listItem.textContent = `${user.username} - ${user.pass}`;
-        list.appendChild(listItem);
-    });
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
-
-getUser();
